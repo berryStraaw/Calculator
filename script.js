fdf940423bd4bf8c45f847858d69b8437cb30d17
berryStraw <marius1357975@gmail.com>
2021-10-16 14:58:13 +0100

    basic calculator not finished, gui set up, add sidebar, add more functions, fix .

--- script.js
+++ script.js
@@ -58,12 +58,85 @@ function opp(sign, a, b){
     if(sign=="/"){
         return divide(a,b);
     }
-    if(sign=="**"){
+    if(sign=="^"){
         return power(a,b);
     }
     if(sign=="!"){
         return factorial(a);
     }
 }
+function buildStr(sign, a, b){
+    text1.textContent=a+" "+sign+" " + b;
+}
+function clear(){
+        a='';
+        b='';
+        sign='';
+        text1.textContent='';
+        text2.textContent='';
+}
+function addStr(id, num){
+    let [trash, numCheck]= num.split(' ');
+    /* text1.textContent=text1.textContent + id + numCheck; */
+    if(text2.textContent!=''){
+        clear();
+        if(!numCheck){
+            return; 
+        }
+    }
+    if(id=="AC"){
+        clear();
+        return;
+    }
+    if(id=="="){
+        text2.textContent=opp(sign,a,b);
+        return;
+    }
+    
+    if(numCheck=='num'){
+        if(a==""){
+            a=id;
+        }
+        else{
+            if(sign!=""){
+                if(b==""){
+                    b=id;
+                }
+                else{
+                    b+=id;
+                }
+            }
+            else{
+                a+=id;
+            } 
+        }   
+    }      
+    if(!numCheck){
+        if(a==""){
+          text1.textContent="please enter a number first";  
+          return;
+        }
+        else{
+           sign=id; 
+        }
+    }
+    buildStr(sign, a, b);
+}
+/* console.log(opp("!",4)); */
 
-console.log(opp("!",4));
+const buttonBox=document.querySelector('.buttonBox');
+const buttons=buttonBox.querySelectorAll('button');
+const text1=document.querySelector('.text1');
+const text2=document.querySelector('.text2');
+let a='';
+let b='';
+let sign='';
+text1.textContent='';
+text2.textContent='';
+buttons.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        addStr(e.target.id, e.target.className);
+        /* text1.textContent=text1.textContent + e.target.id; */
+    });
+});
+
